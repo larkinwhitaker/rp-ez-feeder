10f42149700577e55e9ca48e788ab160cab3a242
Larkin Whitaker <lwhitaker@keystrokeconsulting.com>
2023-12-15 17:49:49 -0500

    Bug Fixes

--- controls.py
+++ controls.py
@@ -8,10 +8,10 @@ from settings import getSettings, updateFeedingTime, toggleEnabled
 ROWS = 4
 COLS = 4
 keys =  [
-	'1','2','3','A',    #key code
-	'4','5','6','B',
-	'7','8','9','C',
-	'*','0','#','D'
+    '1','2','3','A',    #key code
+    '4','5','6','B',
+    '7','8','9','C',
+    '*','0','#','D'
 ]
 
 rowsPins = [12,16,18,22]
@@ -55,61 +55,61 @@ def displayedEditTime(timeStr):
         return '{military_time[:2]}:00'
     elif len(timeStr) == 3:
         return '{military_time[:2]}:{military_time[:-1]}0'
-    else
+    else:
         return '{military_time[:2]}:{military_time[:-2]}'
 
 def loop(isEditing):
-	keypad = Keypad.Keypad(keys,rowsPins,colsPins,ROWS,COLS) #creat Keypad object
-	keypad.setDebounceTime(50)
+    keypad = Keypad.Keypad(keys,rowsPins,colsPins,ROWS,COLS) #creat Keypad object
+    keypad.setDebounceTime(50)
     mcp.output(3,1) # turn on LCD backlight 
     lcd.begin(16,2) # set number of LCD lines and columns
 
-    settings = getSettings()
-    if isEditing:
-        display(displayedEditTime(timeSetting))
-    else:
-        if not settings['enabled']:
-            display("Disabled")
-        elif not settings['feed_at_hour'] or not settings['feed_at_minute']:
-            display("Select time")
+    while(True):
+        settings = getSettings()
+        if isEditing:
+            display(displayedEditTime(timeSetting))
         else:
-            display("{settings['feed_at_hour']}:{settings['feed_at_minute']}")
+            if not settings['enabled']:
+                display("Disabled")
+            elif not settings['feed_at_hour'] or not settings['feed_at_minute']:
+                display("Select time")
+            else:
+                display("{settings['feed_at_hour']}:{settings['feed_at_minute']}")
 
-	timeSetting = ''
-	while(True):
+        timeSetting = ''
         key = keypad.getKey()
         if(key == keypad.NULL):
-        	continue
+            continue
         elif key == 'A'
             if not isEditing:
                 isEditing = True
             elif not is_valid_military_time(timeSetting):
-            	display('Invalid time, please clear & try again')
+                display('Invalid time, please clear & try again')
             else
                 # save time, end editing
                 minute = parse_minute(timeSetting)
                 hour = parse_hour(timeSetting)
                 updateFeedingTime(hour, minute)
                 isEditing = False
-        	
+            
         elif key == 'B'
-        	# cancel select time
+            # cancel select time
             isEditing = False
             timeSetting = ''
 
         elif key == 'C'
-        	# clear select time
+            # clear select time
             timeSetting = ''
 
         elif key == 'D'
-        	# enable/disable
+            # enable/disable
             toggleEnabled()
 
         else
             if len(timeSetting) >= 4:
                 print ("You Pressed Ignored Key: {key}")
                 continue
-        	timeSetting = "{timeSetting}{key}"
+            timeSetting = "{timeSetting}{key}"
             print ("You Pressed Key: {key}")
 
 
@@ -131,9 +131,9 @@ lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
 
 
 if __name__ == '__main__':
-	print ("Program is starting ...")
+    print ("Program is starting ...")
 
-	try: 
+    try: 
         loop(isEditing)
-	except KeyboardInterrupt:
-	    destroy()
+    except KeyboardInterrupt:
+        destroy()
